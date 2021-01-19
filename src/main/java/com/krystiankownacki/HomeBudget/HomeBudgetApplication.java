f7f86dc33f3280106d739683115d384d408c8196
Krystian Kownacki <krystiankownacki@gmail.com>
2021-01-19 17:30:06 +0100

    Init commit

---
+++ src/main/java/com/krystiankownacki/HomeBudget/HomeBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.krystiankownacki.HomeBudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeBudgetApplication.class, args);
+	}
+
+}
